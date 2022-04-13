41c57b918af11dd600d5f5362c6286748638b8ce
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-04-13 15:58:46 +0500

    insert data in studet table

---
+++ DML/Insert/insert data in student table.sql
@@ -0,0 +1,12 @@
+INSERT INTO student (Name,Class,Address,Grade,marks) values
+ ('ABOBAKAR','BSIT','Wazirabad','A+',95),
+('Tayyab','BSIT','Gujrat','A+',99),
+('Hamza','BSIT','Lala musa','A',91),
+('Mahnoor','BSIT','Gujrat','B+',85),
+('Zarbab','BSIT','Gujrat','B',75),
+('ABUBAKAR','BSIT','Wazirabad','A',90),
+('Saira','BSIT','Mandi','B-',70),
+('Usama','BSIT','Gujrat','A',90),
+('Sha Jahan','BSIT','Gujrat','B+',80),
+('Sulman','BSIT','Jehlam','C',60),
+('Saad','BSIT','Jehlam','C+',66)
